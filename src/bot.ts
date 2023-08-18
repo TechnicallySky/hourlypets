15bdca29aef3d02537dd6cff54d05d929851b3de
Skylar Caulfield <skylarcaulfeild@gmail.com>
2023-08-18 18:43:01 -0500

    declare api setup via dotenv

---
+++ src/bot.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+import * as petfinder from '@petfinder/petfinder-js';
+
+
+const petfinder_api_key = process.env.petfinder_api_key;
+const petfinder_api_secret = process.env.petfinder_api_secret;
+const mastodon_api_token = process.env.mastodon_api_token;
+//For Future instagram support 
+const instagram_graph_token = process.env.instagram_graph_token
