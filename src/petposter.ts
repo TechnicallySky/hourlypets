bdcd12080a452533fbb09e6d43fd2e29cce7b3a9
SkyMTF <skylarcaulfeild@gmail.com>
2023-09-07 19:00:52 -0500

    Change the client from a login function to constructor

--- src/petposter.ts
+++ src/petposter.ts
@@ -1,23 +1,25 @@
 import * as dotenv from 'dotenv';
 import { Client } from "@petfinder/petfinder-js";
+import { Animal } from '@petfinder/petfinder-js/dist/api/animal';
 dotenv.config();
 
 
-const petfinder_api_key = process.env.PETFINDER_API_KEY as string;
-const petfinder_api_secret = process.env.PETFINDER_API_SECRET as string;
 const animal_type = process.env.ANIMAL_TYPE;
 
 //Simple class for grabbing a pet. 
 
 export class PetPoster {
-    async login() {
-        const client = new Client({ apiKey: petfinder_api_key, secret: petfinder_api_secret});
-        return client;
+    readonly client: Client;
+    constructor(){
+        this.client = new Client({apiKey:process.env.PETFINDER_API_KEY as string, secret: process.env.PETFINDER_API_SECRET as string})
     }
     async petlookup() {
-        const client = await this.login();
-        const animals = await client.animal.search({type:animal_type, limit:1, sort:"random"})
+        const animals = await this.client.animal.search({type:animal_type, limit:1, sort:"random"})
         return animals.data.animals[0];
+    }
+    async organizationlookup(orgid : string) {
+        const org = await this.client.organization.show(orgid);
+        return org.data.organization;
     }
 }
 
