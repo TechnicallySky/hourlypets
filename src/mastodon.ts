5c5e247ffb2eedea3aea6e2b85f94aeb2a39f34b
SkyMTF <skylarcaulfeild@gmail.com>
2023-09-24 16:10:44 -0500

    Rewrite to support MastoJS and fix images

--- src/mastodon.ts
+++ src/mastodon.ts
@@ -1,30 +1,44 @@
-import * as Mastodon from 'tsl-mastodon-api';
 import * as dotenv from 'dotenv';
 import { PetPoster } from './petposter.js';
+import { createRestAPIClient } from "masto";
+import axios, { AxiosResponse } from 'axios';
 dotenv.config();
 
 
 
-const Mastodon_api_token =  process.env.MASTODON_API_KEY as string; 
-const Mastodon_api_url = process.env.MASTODON_API_URL as string;
-console.log(Mastodon_api_url);
 const petposter = new PetPoster;
 
-export class HourlyPoster {
-    async Login() {
-        const client = new Mastodon.API({
-            access_token: Mastodon_api_token,
-            api_url: Mastodon_api_url,
-        })
-        return client;
-    }
-    async Post(post_content: string){
-        const client = await this.Login();
+export class MastodonPoster {
+    readonly client: ReturnType<typeof createRestAPIClient>;
+    constructor(){
+        this.client = createRestAPIClient({url: process.env.MASTODON_API_URL as string, accessToken: process.env.MASTODON_API_KEY as string})}
+    async MediaUpload(photos : AxiosResponse["data"][]) {
         try {
-            const result = await client.postStatus({status: post_content})
+            const full_photo_urls : string[] = photos.map((item) => item.full);
+            const media_ids : string[] = [];
+            let i = 0;
+            for (const photo of full_photo_urls){
+                media_ids.push(await this.client.v1.media.create({file: await fetch(photo).then(res => res.blob())}).then(res => res.id))
+            }
+            return media_ids;
+    }
+        catch(error) {
+            console.error(error);
+        }
+    }
+    async Post(animal : AxiosResponse["data"]) {
+        console.log(await petposter.organizationlookup(animal.organization_id).then(res => res.name))
+        try {
+            this.client.v1.statuses.create({
+                status: animal.name + animal.description && `\n\n${animal.description}` + "\n\n" + animal.name + " is at " + await petposter.organizationlookup(animal.organization_id).then(res => res.name) 
+                + " in " + animal.contact.address.city + " " + animal.contact.address.state + "\n\n" + animal.url,
+                visibility:"public",
+                mediaIds: await this.MediaUpload(animal.photos)
+            })
+
         }
         catch (error) {
-            console.error("Error posting to Mastodon: " + JSON.stringify(error));
+            console.error(error)
         }
     }
-}
+}   
